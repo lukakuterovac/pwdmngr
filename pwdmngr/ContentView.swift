92d43f180d1967af8fce15d5610daaf188df0c75
Luka Kuterovac <luka.kuterovac@gmail.com>
2024-06-03 21:29:09 +0200

    Create project

---
+++ pwdmngr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pwdmngr
+//
+//  Created by Luka Kuterovac on 03.06.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pwdmngr/pwdmngrApp.swift
@@ -0,0 +1,17 @@
+//
+//  pwdmngrApp.swift
+//  pwdmngr
+//
+//  Created by Luka Kuterovac on 03.06.2024..
+//
+
+import SwiftUI
+
+@main
+struct pwdmngrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
