e5d05f1898fd94ddcaf3bac6cf8af9dceab11785
Luka Kuterovac <luka.kuterovac@gmail.com>
2024-06-30 17:03:05 +0200

    Save generator options to local storage

--- pwdmngr/Helpers/GeneratorOptions.swift
+++ pwdmngr/Helpers/GeneratorOptions.swift
@@ -10,21 +10,59 @@ import SwiftUI
 
 final class GeneratorOptions: ObservableObject {
     private static let shared = GeneratorOptions()
-    @Published var passwordLength: Double
-    @Published var includeLowercase: Bool
-    @Published var includeUppercase: Bool
-    @Published var includeNumbers: Bool
-    @Published var includeSpecialCharacters: Bool
+    
+    @Published var passwordLength: Double {
+        didSet {
+            UserDefaults.standard.set(passwordLength, forKey: "passwordLength")
+        }
+    }
+    @Published var includeLowercase: Bool {
+        didSet {
+            UserDefaults.standard.set(includeLowercase, forKey: "includeLowercase")
+        }
+    }
+    @Published var includeUppercase: Bool {
+        didSet {
+            UserDefaults.standard.set(includeUppercase, forKey: "includeUppercase")
+        }
+    }
+    @Published var includeNumbers: Bool {
+        didSet {
+            UserDefaults.standard.set(includeNumbers, forKey: "includeNumbers")
+        }
+    }
+    @Published var includeSpecialCharacters: Bool {
+        didSet {
+            UserDefaults.standard.set(includeSpecialCharacters, forKey: "includeSpecialCharacters")
+        }
+    }
     
     private init() {
-        passwordLength = 6
-        includeLowercase = true
-        includeUppercase = false
-        includeNumbers = false
-        includeSpecialCharacters = false
+        self.passwordLength = UserDefaults.standard.double(forKey: "passwordLength")
+        self.includeLowercase = UserDefaults.standard.bool(forKey: "includeLowercase")
+        self.includeUppercase = UserDefaults.standard.bool(forKey: "includeUppercase")
+        self.includeNumbers = UserDefaults.standard.bool(forKey: "includeNumbers")
+        self.includeSpecialCharacters = UserDefaults.standard.bool(forKey: "includeSpecialCharacters")
+        
+        if UserDefaults.standard.object(forKey: "passwordLength") == nil {
+            self.passwordLength = 6
+        }
+        if UserDefaults.standard.object(forKey: "includeLowercase") == nil {
+            self.includeLowercase = true
+        }
+        if UserDefaults.standard.object(forKey: "includeUppercase") == nil {
+            self.includeUppercase = false
+        }
+        if UserDefaults.standard.object(forKey: "includeNumbers") == nil {
+            self.includeNumbers = false
+        }
+        if UserDefaults.standard.object(forKey: "includeSpecialCharacters") == nil {
+            self.includeSpecialCharacters = false
+        }
     }
     
     static func getInstance() -> GeneratorOptions {
         return shared
     }
 }
+
